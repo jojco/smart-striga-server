9cc374e3b8b19bcf7a0c7e8676f8c558e2d6b6b6
jozef.janecek <jozef.janecek@asseco-ceit.com>
2022-12-06 19:28:00 +0000

    minnor changes

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,27 +1,15 @@
 import { Injectable } from '@nestjs/common';
-const {SCD30} = require('scd30-node');
-const sensor = require('ds18b20-raspi');
 
-
-
-sensor.readAllC((err, temps) => {
-	if (err) {
-		console.log(err);
-	} else {
-		console.log(temps);
-	}
-});
-
+import { SCD30 } from 'scd30-node';
+import sensor from 'ds18b20-raspi';
 
 (async () => {
   const scd30 = await SCD30.connect();
   await scd30.startContinuousMeasurement();
- 
   const measurement = await scd30.readMeasurement();
   console.log(`CO2 Concentration: ${measurement.co2Concentration} ppm`);
   console.log(`Temperature: ${measurement.temperature} °C`);
   console.log(`Humidity: ${measurement.relativeHumidity} %`);
- 
   await scd30.disconnect();
 })();
 
@@ -29,5 +17,14 @@ sensor.readAllC((err, temps) => {
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+  getTemperature(): string {
+    sensor.readAllC((err, temps) => {
+      if (err) {
+        return err;
+      } else {
+        return temps;
+      }
+    });
   }
 }
